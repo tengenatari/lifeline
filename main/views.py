0168710c4e37d5cc91a28ec4fc51c44c41eb19c2
John Lenin <ziziz123ziziz@gmail.com>
2025-01-06 18:03:42 +0500

    add favicon

--- main/views.py
+++ main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from django.conf import settings
+from django.http import FileResponse, HttpRequest, HttpResponse
+from django.views.decorators.cache import cache_control
+from django.views.decorators.http import require_GET
 
 def index(request):
     return render(request, 'base.html')
+
+@require_GET
+@cache_control(max_age=60 * 60 * 24, immutable=True, public=True)  # one day
+def favicon(request: HttpRequest) -> HttpResponse:
+    file = (settings.BASE_DIR / "static" / "images" / "favicon.png").open("rb")
+    return FileResponse(file)
+
