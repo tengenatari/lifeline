d1cb3298c418d6ec198813a2a9ffb36ac0063c6d
John Lenin <ziziz123ziziz@gmail.com>
2025-01-09 23:48:32 +0500

    add jinga2

--- main/views.py
+++ main/views.py
@@ -5,7 +5,18 @@ from django.views.decorators.cache import cache_control
 from django.views.decorators.http import require_GET
 
 def index(request):
-    return render(request, 'index.html')
+    return news(request)
+
+def players(request):
+    return render(request, 'players.html')
+def tournaments(request):
+    return render(request, 'tournaments.html')
+def news(request):
+    return render(request, 'news.html')
+def clubs(request):
+    return render(request, 'clubs.html')
+def about(request):
+    return render(request, 'about.html')
 
 @require_GET
 @cache_control(max_age=60 * 60 * 24, immutable=True, public=True)  # one day
