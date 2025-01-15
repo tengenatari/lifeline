623b1805ad7825633c54d0ffc6f182c005195bc9
John Lenin <ziziz123ziziz@gmail.com>
2025-01-15 21:25:15 +0500

    upd players

--- lifeline/urls.py
+++ lifeline/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('news/', include('news.urls')),
+    path('players/', include('players.urls')),
 ]
 
 
--- players/views.py
+++ players/views.py
@@ -1,4 +1,12 @@
+from django.conf import settings
+import csv
 from django.shortcuts import render
 
+
 def index_players(request):
-    return render(request, 'players/players.html')
+    csv_file_path = settings.MEDIA_ROOT / 'csv' / 'tyumen_players.csv'
+    with open(csv_file_path, 'r',  encoding="utf-8") as file:
+        data = [{k: str(v) for k, v in row.items()} \
+        for row in csv.DictReader(file, skipinitialspace=True)]
+
+    return render(request, 'players/players.html', {'data': data})
