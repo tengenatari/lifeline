1476f5caae78b3206343b5ca29b212dc6c4ea0e1
John Lenin <ziziz123ziziz@gmail.com>
2025-01-19 20:07:52 +0500

    upd

--- players/models.py
+++ players/models.py
@@ -4,11 +4,14 @@ from django.db import models
 
 
 class Player(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
     name = models.CharField(max_length=100, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
     rank = models.CharField(max_length=100, blank=True, null=True)
     tournaments = models.CharField(max_length=100, blank=True, null=True)
     games = models.CharField(max_length=100, blank=True, null=True)
+    last_game = models.DateField
+    deleted = models.BooleanField(default=False, null=False)
 
     def __str__(self):
         return self.name
