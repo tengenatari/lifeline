b46a7c9046884eb7ada050d64841c752aea0604e
Andrey <ssa25t@mail.ru>
2025-01-20 17:57:49 +0500

    added last 3 years

--- players/views.py
+++ players/views.py
@@ -1,6 +1,11 @@
 from django.conf import settings
 from django.shortcuts import render
 from .models import Player
-
+import datetime
 def index_players(request):
-    return render(request, 'players/players.html', context={'players': Player.objects.order_by('-rating')})
+    return render(request, 'players/players.html',
+                  context={'players': Player.objects.filter(
+                      last_game_date__range=[datetime.datetime.now() - datetime.timedelta(days=3*365),
+                                             datetime.date.today()]).order_by('-rating')
+                           }
+                  )
