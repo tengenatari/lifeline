98dff84207c504f8e9057b1b594be853c9da9d39
John Lenin <ziziz123ziziz@gmail.com>
2025-01-20 16:58:44 +0500

    fix player_parser

--- players/scripts/player_parser.py
+++ players/scripts/player_parser.py
@@ -4,19 +4,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
-import pandas as pd
 from ..models import Player
 
 
-def get_players_df(driver, valid_city):
+def parse_players_id(driver, valid_city):
     url = "https://gofederation.ru/players"
     select_xpath = '/html/body/div/div/div[2]/div/div/div[1]/div[1]/select'
     table_xpath = '/html/body/div/div/div[2]/div/div/div[1]/div[2]/div/table'
-
     driver.get(url)
-
     select_element = Select(driver.find_element(By.XPATH, select_xpath))
     
+    players_id = set()
     if "За всё время" in [o.text for o in select_element.options]:
         select_element.select_by_visible_text("За всё время")
         
@@ -28,76 +26,62 @@ def get_players_df(driver, valid_city):
         table = soup.find("tbody")
         rows = table.find_all('tr')
         
-        df = pd.DataFrame({"name": [], "city": []})
         for row in rows:
             cells = row.find_all('td')
             id = cells[1].find('a', {"class": ""})["href"].split('/')[-1]
-            name = cells[1].find('a', {"class": ""}).text
             city = cells[2].text
-            if name == "Raimberdiev Baytik":
-                print(row)
-            df.loc[id] = [name, city]
-        
-        df.to_csv('test.csv')
-        df["name"] = df["name"].apply(lambda s: s[:(s.find("_") if s.find("_")>0 else len(s))])
-        
-        df = df.query('city in @valid_city')
-        return df 
-
-    return None
-
-
-def get_player_info(driver, url):    
-    try:
-        driver.get(url)
-        raiting = driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[2]/div[1]/div[2]").text
-        rank = driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[2]/div[2]/div[2]").text
-        tournaments = driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[2]/div[3]/div[2]").text
-        games = driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[2]/div[4]/div[2]").text
-
-        rank = rank.replace('d', ' дан').replace('k', ' кю')
-    except:
-        raiting = None
-        rank = None
-        tournaments = None
-        games = None
-    return raiting, rank, tournaments, games
+            if city in valid_city:
+                players_id.add(id)
+    return players_id
 
 
 def get_player_url(id):
     return f"https://gofederation.ru/players/{id}"
 
 
+def get_player_info(driver, url):    
+    try:
+        driver.get(url)
+        info = {
+            "name": driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[1]").text,
+            "city": driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[1]/span").text,
+            "raiting": driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[2]/div[1]/div[2]").text,
+            "rank": driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[2]/div[2]/div[2]").text,
+            "tournaments": driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[2]/div[3]/div[2]").text,
+            "games": driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div/div/div[2]/div[4]/div[2]").text,
+        }
+        if info["name"].find('_') > 0:
+            info["name"] = info["name"][:info["name"].find('_')]
+        if info["name"].find('(') > 0:
+            info["name"] = info["name"][:info["name"].find(' (')]
+        info["city"] = info["city"].replace('(', '').replace(')', '')
+        info["rank"] = info["rank"].replace('d', ' дан').replace('k', ' кю')
+    except:
+        info = None
+    return info
+
+
+def update_player(driver, id, valid_city, new_player):
+    url = get_player_url(id)
+    player_info = get_player_info(driver, url)
+    if player_info is not None:
+        if new_player and (not player_info["city"] in valid_city):
+            return False
+        else:
+            player, created = Player.objects.update_or_create(id=id, defaults=player_info)
+            return created
+        
+
 def run(*args):
     valid_city = ["Тюмень"]
     driver = webdriver.Chrome()
-    df = get_players_df(driver, valid_city)
-    if df is None:
-        return
+
+    new_players_set = parse_players_id(driver, valid_city)
+    old_players_set = set(Player.objects.values_list('id', flat=True))
+    id_set = new_players_set.union(old_players_set)
+
+    for id in id_set:
+        new_player = (id in new_players_set) and not (id in old_players_set)
+        update_player(driver, id, valid_city, new_player)
     
-    df = df.dropna()
-    #df.to_csv('test.csv')
-    # for id, row in df.iterrows():
-    #     url = get_player_url(id)
-    #     raiting, rank, tournaments, games = get_player_info(driver, url)
-    #     df.loc[id, "raiting"] = raiting
-    #     df.loc[id, "rank"] = rank
-    #     df.loc[id, "tournaments"] = tournaments
-    #     df.loc[id, "games"] = games
-
-    #     if raiting is not None:
-    #         update_dict = {"name": row["name"], 
-    #                        "city": row["city"], 
-    #                        "rank": rank, 
-    #                        "tournaments": tournaments, 
-    #                        "games": games,
-    #                        "raiting": raiting}
-            
-    #         player, created = Player.objects.update_or_create(id=id,  defaults=update_dict)
-            
-    driver.quit()  
-    print(df)
-
-
-# if __name__ == '__main__':
-#     main()
+    driver.quit()
