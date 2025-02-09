e9ab9eacba01671c01961966967a585f609871b4
John Lenin <ziziz123ziziz@gmail.com>
2025-02-09 23:20:12 +0500

    bug fix

--- clubs/models.py
+++ clubs/models.py
@@ -5,7 +5,8 @@ from django.db import models
 
 class Club(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(default="")
+    logo = models.FileField(upload_to='images/clubs/', default='static/images/clubs/club-image.svg', verbose_name='Логотип')
+    description = models.TextField(default="", verbose_name='Описание')
     vk = models.CharField(default="", blank=True, max_length=200, verbose_name='Ссылка на vk')
     telegram = models.CharField(default="", blank=True, max_length=200, verbose_name='Ссылка на telegram')
     ogs = models.CharField(default="", blank=True, max_length=200, verbose_name='Ссылка на ogs')
--- tournaments/models.py
+++ tournaments/models.py
@@ -5,8 +5,8 @@ class Tournament(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, blank=True, null=True, verbose_name='Название')
     city = models.CharField(max_length=100, blank=True, null=True, verbose_name='Город')
-    period = models.CharField(max_length=100, blank=True, null=True, verbose_name='Сроки проведения')
-    date = models.DateField(default='2000-01-01', blank=True, verbose_name='Дата последней игры')
+    period = models.CharField(max_length=100, blank=True, null=True, verbose_name='Сроки проведения (произвольный текст)')
+    date = models.DateField(default='2000-01-01', blank=True, verbose_name='Дата турнира (определяет сортировку)')
     url = models.CharField(max_length=200, blank=True, null=True, verbose_name='Ссылка')
     is_visible = models.BooleanField(default=True, verbose_name='Отображать')
 
